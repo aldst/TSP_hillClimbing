bba47e66eeef67dd2cad7cee476a4fdb825b81dd
dredson71 <dredson71@gmail.com>
2020-04-15 17:27:33 -0500

    FixedAlgorithm

--- TA.py
+++ TA.py
@@ -5,7 +5,7 @@ import random
 
 
 def calc_dist(city1, city2):
-    return math.sqrt((city2[0] - city1[0]) ** 2 + (city2[1] - city1[1]) ** 2)
+    return math.sqrt((city2[1] - city1[1]) ** 2 + (city2[2] - city1[2]) ** 2)
 
 
 class Route:
@@ -13,11 +13,13 @@ class Route:
 
     def __init__(self, cities):
         self.cities = cities
-        random.shuffle(cities)
+        listAux = cities[1:len(cities)]
+        random.shuffle(listAux)
+        cities[1:len(cities)] = listAux[0:len(cities)]
 
-    def changingRoute(self, cities):
-        for index in cities:
-            self.cities[index] = cities
+    def changingRoute(self, citiesNew):
+        for index in range(len(citiesNew)):
+            self.cities[index] = citiesNew[index]
 
     def getCities(self):
         return self.cities
@@ -25,45 +27,60 @@ class Route:
     def swapCities(self, pos1, pos2):
         aux = self.cities[pos1]
         self.cities[pos1] = self.cities[pos2]
-        self.cities[pos2] = self.cities[pos1]
+        self.cities[pos2] = aux
 
     def getTotalDistance(self):
         citiesSize = len(self.cities)
         totalDistance = 0
-        for index in citiesSize:
+        for index in range(citiesSize):
             if index < citiesSize - 1:
                 totalDistance += calc_dist(self.cities[index], self.cities[index + 1])
         totalDistance += calc_dist(self.cities[citiesSize - 1], self.cities[0])
         return totalDistance
 
 
-class HillClimbing:
-    def findShortestRoute(self, route):
-        iterationCounter = 0
-        while iterationCounter < 100:
-            adyacent = self.obtainAdjacentRoute(Route(route))
-            if adyacent.getTotalDistance() <= route.getTotalDistance():
-                route = Route(adyacent)
-            else:
-                iterationCounter += 1
-        return route
+def obtainAdjacentRoute(route):
+    x1 = 0
+    x2 = 0
+    while x1 == x2:
+        x1 = int(len(route.getCities()) * random.uniform(0, 1))
+        x2 = int(len(route.getCities()) * random.uniform(0, 1))
+        if x1 == 0 or x2 == 0:
+            x1 = 0
+            x2 = 0
+    route.swapCities(x1, x2)
+    return route
 
-    def obtainAdjacentRoute(self, route=Route()):
-        x1, x2 = 0
-        while x1 == x2:
-            x1 = len(self.getCities()) * random.uniform(0, 1)
-            x2 = len(self.getCities()) * random.uniform(0, 1)
-        city1 = self.getCities()[x1]
-        city2 = self.getCities()[x2]
-        self.swapCities(city1, city2)
-        return route
+
+def findShortestRoute(route):
+    iterationCounter = 0
+    while iterationCounter < 2:
+        adyacent = obtainAdjacentRoute(Route(data_Read()))
+        print(adyacent.getTotalDistance())
+        print(route.getTotalDistance())
+        if adyacent.getTotalDistance() <= route.getTotalDistance():
+            iterationCounter = 0
+            print(route.getCities())
+            print(adyacent.getCities())
+            route.changingRoute(adyacent.getCities())
+            print(route.getCities())
+        else:
+            iterationCounter += 1
+        print(route.getCities())
+        print(route.getTotalDistance())
+    return route
 
 
 def data_Read():
     excel_file = 'ciudades.xlsx'
     data = pd.read_excel(excel_file)
     cities = list(data.set_index('CODIGO').values)
-    print(cities)
+
+    return cities
 
 
 data_Read()
+route = Route(data_Read())
+routeaux = Route(data_Read())
+route2 = obtainAdjacentRoute(routeaux)
+findShortestRoute(route)
